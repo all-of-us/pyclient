82b3d491bb712d333336c218b167f81a495ddb0c
Dan Rodney <danrodney@danrodney-goobuntu.aus.corp.google.com>
2018-04-13 13:05:04 -0500

    materialize cohort now does multiple requests

--- py/aou_workbench_client/cohorts.py
+++ py/aou_workbench_client/cohorts.py
@@ -2,7 +2,7 @@ from aou_workbench_client.auth import get_authenticated_swagger_client
 from aou_workbench_client.config import all_of_us_config
 from aou_workbench_client.swagger_client.apis.cohorts_api import CohortsApi
 
-def materialize_cohort(request):
+def materialize_cohort_page(request):
     """Materializes a cohort in the workspace containing this notebook, based
     on the provided MateralizedCohortRequest."""
     client = get_authenticated_swagger_client()
@@ -10,3 +10,23 @@ def materialize_cohort(request):
     return cohorts_api.materialize_cohort(all_of_us_config.workspace_namespace,
                                           all_of_us_config.workspace_id,
                                           request=request)
+
+def materialize_cohort(request, max_results=None):
+    """Materializes a cohort in the workspace containing this notebook, based
+    on the provided MateralizedCohortRequest."""
+    client = get_authenticated_swagger_client()
+    cohorts_api = CohortsApi(api_client=client)
+    num_results = 0
+    while True:
+      response = cohorts_api.materialize_cohort(all_of_us_config.workspace_namespace,
+                                                all_of_us_config.workspace_id,
+                                                request=request)
+      for result in response.results:
+        yield result
+        num_results += 1
+        if num_results >= max_results:
+          return
+      if response.next_page_token:
+        request.page_token = response.next_page_token
+      else:
+        return
