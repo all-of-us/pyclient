4c8eb652cb0df8b2d813b58f810c89f0139095aa
danqrodney <danrodney@google.com>
2018-04-13 16:40:17 -0500

    Updating materialize_cohort to return a generator of dicts. (#10)
    * materialize cohort now does multiple requests
    
    * Update logic
    
    * Debuggin
    
    * Update
    
    * Changes
    
    * Updating doc
    
    * Updating docs again
    
    * Handling page size not set
    
    * Fixing

--- py/aou_workbench_client/cohorts.py
+++ py/aou_workbench_client/cohorts.py
@@ -1,12 +1,46 @@
 from aou_workbench_client.auth import get_authenticated_swagger_client
 from aou_workbench_client.config import all_of_us_config
 from aou_workbench_client.swagger_client.apis.cohorts_api import CohortsApi
+from copy import deepcopy
 
-def materialize_cohort(request):
-    """Materializes a cohort in the workspace containing this notebook, based
-    on the provided MateralizedCohortRequest."""
+def materialize_cohort_page(request):
+    """Returns a MaterializeCohortResponse representing a page of results 
+    from materializing a cohort in the workspace containing this notebook, based
+    on the provided MateralizeCohortRequest."""
     client = get_authenticated_swagger_client()
     cohorts_api = CohortsApi(api_client=client)
     return cohorts_api.materialize_cohort(all_of_us_config.workspace_namespace,
                                           all_of_us_config.workspace_id,
                                           request=request)
+
+def materialize_cohort(request, max_results=None):
+    """Materializes a cohort in the workspace containing this notebook, based
+    on the provided MateralizeCohortRequest. Returns a generator of 
+    dictionaries containing the results.
+    
+    If max_results is specified, up to that many results will be returned; 
+    if not, all available results will be returned. 
+    
+    Multiple server requests may be made to retrieve all the results,
+    using the page_size specified in the request for each request."""
+    client = get_authenticated_swagger_client()
+    cohorts_api = CohortsApi(api_client=client)
+    num_results = 0
+    # Clone the request, since we're going to be modifying it.
+    request = deepcopy(request)    
+    request.page_size = request.page_size or 1000
+    while True:
+      if max_results and (max_results - num_results) < request.page_size:
+        request.page_size = max_results - num_results   
+      response = cohorts_api.materialize_cohort(all_of_us_config.workspace_namespace,
+                                                all_of_us_config.workspace_id,
+                                                request=request)      
+      for result in response.results:        
+        yield result
+        num_results += 1
+        if max_results and num_results >= max_results:
+          return
+      if response.next_page_token:
+        request.page_token = response.next_page_token        
+      else:
+        return
