9224fc0253cf6cc9ded6b61d7cc818e40d8781c0
Dan Rodney <danrodney@danrodney-goobuntu.aus.corp.google.com>
2018-05-29 08:47:21 -0500

    Adding concept code

--- py/aou_workbench_client/concepts.py
+++ py/aou_workbench_client/concepts.py
@@ -1,14 +1,9 @@
 from aou_workbench_client.auth import get_authenticated_swagger_client
 from aou_workbench_client.config import all_of_us_config
 from aou_workbench_client.swagger_client.apis.concepts_api import ConceptsApi
-<<<<<<< HEAD
-
-from IPython.display import display
-=======
 import pandas as pd
 
 from IPython.display import display, HTML
->>>>>>> f5d0e53964dff4646c6e982dfeb8e88ee09b2838
 
 def search_concepts(request):
   client = get_authenticated_swagger_client()
@@ -18,34 +13,10 @@ def search_concepts(request):
                                           request=request)
   return response.items
 
-<<<<<<< HEAD
-def get_concepts_frame(request):
-  concepts = search_concepts(request)
-  concepts_frame = pd.DataFrame(concepts, columns=["concept_id",
-                                                   "concept_name",
-                                                   "domain_id",
-                                                   "vocabulary_id",
-                                                   "concept_code",
-                                                   "concept_class_id",
-                                                   "standard_concept",
-                                                   "count_value"])
-  concepts_frame.rename(columns={"concept_id": "ID",
-                                 "concept_name": "Name",
-                                 "domain_id": "Domain",
-                                 "vocabulary_id": "Vocabulary",
-                                 "concept_code": "Code",
-                                 "concept_class_id": "Class",
-                                 "standard_concept": "Standard?",
-                                 "count_value": "Count"})
-  return concepts_frame
-
-def display_concepts(request):
-  concepts_frame = get_concepts_frame(request)
-  display(concepts_frame)
-=======
 def get_concept_dict(concept):
   return { "ID": concept.concept_id,
            "Name": concept.concept_name,
+           "Code": concept.concept_code,
            "Domain": concept.domain_id,
            "Vocabulary": concept.vocabulary_id,
            "Count": concept.count_value
@@ -54,7 +25,8 @@ def get_concept_dict(concept):
 def get_concepts_frame(request):
   concepts = search_concepts(request)
   return pd.DataFrame([get_concept_dict(concept) for concept in concepts],
-                      columns = ["ID", "Name", "Domain", "Vocabulary", "Count"])
+                      columns = ["ID", "Name", "Code", "Domain", 
+                                 "Vocabulary", "Count"])
 
 def display_concepts(request):
   concepts_frame = get_concepts_frame(request)
@@ -62,5 +34,3 @@ def display_concepts(request):
   s = s.set_table_styles(
       [{"selector": "th", "props": [("text-align", "left")]}])
   display(HTML(s.render()))
-  
->>>>>>> f5d0e53964dff4646c6e982dfeb8e88ee09b2838
