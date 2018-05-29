ad9aac6ca88d52e23edd81cbc9411f473d53d0b7
Dan Rodney <danrodney@danrodney-goobuntu.aus.corp.google.com>
2018-05-29 14:06:00 -0500

    PR comments

--- py/aou_workbench_client/concepts.py
+++ py/aou_workbench_client/concepts.py
@@ -65,43 +65,43 @@ _RESULT_FIELDS = [
 _VOCAB_DICT = {id: id for id in _VOCAB_IDS}
 
 def search_concepts(request):
-  client = get_authenticated_swagger_client()
-  concepts_api = ConceptsApi(api_client=client)
-  response = concepts_api.search_concepts(all_of_us_config.workspace_namespace,
-                                          all_of_us_config.workspace_id,
-                                          request=request)
-  return response.items
+    client = get_authenticated_swagger_client()
+    concepts_api = ConceptsApi(api_client=client)
+    response = concepts_api.search_concepts(all_of_us_config.workspace_namespace,
+                                            all_of_us_config.workspace_id,
+                                            request=request)
+    return response.items
 
 def get_concept_dict(concept):
-  return { f[0]: getattr(concept, f[1]) for f in _RESULT_FIELDS } 
+    return {f[0]: getattr(concept, f[1]) for f in _RESULT_FIELDS} 
 
 def get_concepts_frame(request):
-  concepts = search_concepts(request)
-  return pd.DataFrame([get_concept_dict(concept) for concept in concepts],
-                      columns = [f[0] for f in _RESULT_FIELDS])
+    concepts = search_concepts(request)
+    return pd.DataFrame([get_concept_dict(concept) for concept in concepts],
+                        columns = [f[0] for f in _RESULT_FIELDS])
 
 def display_concepts(request):
-  concepts_frame = get_concepts_frame(request)
-  s = concepts_frame.style.set_properties(**{'text-align': 'left'})
-  s = s.set_table_styles(
-      [{"selector": "th", "props": [("text-align", "left")]}]).hide_index()
-  display(HTML(s.render()))
+    concepts_frame = get_concepts_frame(request)
+    s = concepts_frame.style.set_properties(**{'text-align': 'left'})
+    s = s.set_table_styles(
+        [{"selector": "th", "props": [("text-align", "left")]}]).hide_index()
+    display(HTML(s.render()))
 
 def display_concepts_fn(query, domain, vocabulary, concepts):
-  request = SearchConceptsRequest(query=query)
-  if domain:
-    request.domain = domain
-  if concepts:
-    request.standard_concept_filter = concepts
-  if vocabulary:
-    request.vocabulary_ids = [vocabulary]
-  display_concepts(request)
+    request = SearchConceptsRequest(query=query)
+    if domain:
+        request.domain = domain
+    if concepts:
+        request.standard_concept_filter = concepts
+    if vocabulary:
+        request.vocabulary_ids = [vocabulary]
+    display_concepts(request)
   
-interact = interactive.factory()
-interact_form = interact.options(manual=True, manual_name="Search")  
+    interact = interactive.factory()
+    interact_form = interact.options(manual=True, manual_name="Search")  
   
 def display_concepts_widget():
-  interact_form(display_concepts_fn, query='', domain=_DOMAIN_DICT,
-                concepts=_STANDARD_CONCEPT_FILTER_DICT,
-                vocabulary=_VOCAB_DICT,
-                manual_name='Search')
+    interact_form(display_concepts_fn, query='', domain=_DOMAIN_DICT,
+                  concepts=_STANDARD_CONCEPT_FILTER_DICT,
+                  vocabulary=_VOCAB_DICT,
+                  manual_name='Search')
