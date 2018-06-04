366f4c7b7f9200d52fdb91ef4d3219ee63c8ec03
danqrodney <danrodney@google.com>
2018-06-04 09:40:48 -0500

    [RW-600] Generate Python cells for materializing cohorts from concept search results. (#25)
    * Rendering own html
    
    * Align left`
    
    * Again
    
    * JS
    
    * Blah`
    
    * Adding form
    
    * Revert
    
    * Revert 2`
    
    * Blah
    
    * AA
    
    * Blah2
    
    * Background color
    
    * Button style
    
    * Bal
    
    * Select
    
    * Row selection
    
    * ommand
    
    * Blah
    
    * Blah 2
    
    * Again
    
    * Blah 3
    
    * WTF
    
    * Whatev
    
    * Foo
    
    * WTF
    
    * Semis
    
    * Newline
    
    * Again
    
    * OK
    
    * materialize
    
    * Semis
    
    * Comma
    
    * Prefix
    
    * Vars
    
    * Bool
    
    * WS
    
    * Cohort name
    
    * JSON
    
    * PR comments
    
    * Fix
    
    * Fix
    
    * Setting lower pandas requirement to avoid issue with numpy in Python 3

--- py/aou_workbench_client/concepts.py
+++ py/aou_workbench_client/concepts.py
@@ -1,3 +1,8 @@
+import json
+
+import html
+import pandas as pd
+from IPython.display import display, HTML
 from aou_workbench_client.auth import get_authenticated_swagger_client
 from aou_workbench_client.config import all_of_us_config
 from aou_workbench_client.swagger_client.apis.concepts_api import ConceptsApi
@@ -5,9 +10,6 @@ from aou_workbench_client.swagger_client.models.domain import Domain
 from aou_workbench_client.swagger_client.models.search_concepts_request import SearchConceptsRequest
 from aou_workbench_client.swagger_client.models.standard_concept_filter import StandardConceptFilter
 from ipywidgets import interactive
-import pandas as pd
-
-from IPython.display import display, HTML
 
 _DOMAIN_DICT = { 
     '': None,
@@ -62,6 +64,130 @@ _RESULT_FIELDS = [
     ('Vocabulary', 'vocabulary_id'),
     ('Count', 'count_value')]
 
+# TODO: prefix IDs, global vars with a unique ID, use them in click handler if 
+# we keep this as a standalone widget (RW-796)
+_CONCEPT_TABLE_HTML_TEMPLATE = """
+<script language="javascript">
+  var selectedRowId = null;
+  var selectedData = null;
+  var oldSelectedColor = null;  
+  
+  function selectConcept(id, name, domain, vocabulary, standard) {
+    maxResults = document.getElementById('max_results');
+    variablePrefix = document.getElementById('variable_prefix');
+    cohortName = document.getElementById('cohort_name');
+    generateCode = document.getElementById('generate_code');
+    maxResults.disabled = false;
+    maxResults.style.color = '';
+    variablePrefix.disabled = false;
+    variablePrefix.style.color = '';
+    cohortName.disabled = false;
+    cohortName.style.color = '';
+    generateCode.disabled = false;
+    if (selectedRowId) {
+      document.getElementById('row_' + selectedRowId).style.backgroundColor = oldSelectedColor;
+    }
+    selectedRowId = id;
+    selectedData = { 'name': name, 'domain': domain, 'vocabulary': vocabulary, 'standard': standard };
+    newSelectedRow = document.getElementById('row_' + id);
+    oldSelectedColor = newSelectedRow.style.backgroundColor;
+    newSelectedRow.style.backgroundColor = '#BBBBFF';    
+  }
+  
+  domainToTableMap = {
+    'Condition': ['ConditionOccurrence', 'condition_concept_id', 'condition_source_concept_id'],
+    'Device': ['DeviceExposure', 'device_concept_id', 'device_source_concept_id'],
+    'Drug': ['DrugExposure', 'drug_concept_id', 'drug_source_concept_id'],
+    'Ethnicity': ['Person', 'ethnicity_concept_id', 'ethnicity_source_concept_id'],
+    'Gender': ['Person', 'gender_concept_id', 'gender_source_concept_id'],
+    'Measurement': ['Measurement', 'measurement_concept_id', 'measurement_source_concept_id'],
+    'Observation': ['Observation', 'observation_concept_id', 'observation_source_concept_id'],
+    'Procedure': ['ProcedureOccurrence', 'procedure_concept_id', 'procedure_source_concept_id'],
+    'Race': ['Person', 'race_concept_id', 'race_source_concept_id']
+  };  
+  
+  function generatePythonCode() {
+    maxResults = document.getElementById('max_results').value;
+    prefix = document.getElementById('variable_prefix').value;
+    cohortName = JSON.stringify(document.getElementById('cohort_name').value);
+    domain = selectedData['domain'];
+    tableData = domainToTableMap[domain];
+    if (!tableData) {
+      throw 'Unsupported domain: ' + domain;
+    }
+    table = tableData[0];
+    if (selectedData['standard']) {
+      column = tableData[1];
+    } else {
+      column = tableData[2];
+    }
+    materializationCode = `
+from aou_workbench_client.swagger_client.models import ResultFilters, MaterializeCohortRequest
+from aou_workbench_client.swagger_client.models import TableQuery, ColumnFilter, FieldSet
+from aou_workbench_client.cohorts import materialize_cohort
+from aou_workbench_client.cdr.model import ${table}
+from IPython.display import display
+import pandas as pd
+    
+# Filter on "${selectedData['name']}" (vocabulary = ${selectedData['vocabulary']}, concept ID = ${selectedRowId})
+${prefix}_filter = ColumnFilter(${table}.${column}, value_number=${selectedRowId})
+${prefix}_query = TableQuery(table=${table}, filters=ResultFilters(column_filter=${prefix}_filter))
+${prefix}_request = MaterializeCohortRequest(cohort_name=${cohortName}, field_set=FieldSet(table_query=${prefix}_query))
+${prefix}_response = materialize_cohort(${prefix}_request, max_results=${maxResults})
+${prefix}_frame = pd.DataFrame(list(${prefix}_response))
+display(${prefix}_frame)`;
+    newCell = IPython.notebook.insert_cell_below('code');
+    newCell.set_text(materializationCode);
+  }
+</script>
+
+<table>
+  <tr>
+    <th>ID</th>
+    <th style="text-align: left">Name</th>
+    <th style="text-align: left">Code</th>
+    <th style="text-align: left">Domain</th>
+    <th style="text-align: left">Vocabulary</th>
+    <th align=right>Count</th>
+  </tr>
+  %s
+</table>
+<table style="background: white">
+   <tr style="background: white">
+     <td style="background: white">Cohort:</td>
+     <!-- TODO: make this a dropdown based on cohorts in the workspace -->
+     <td style="background: white"><input type="text" value="" id="cohort_name" maxlength="80"
+       style="color: #999999" disabled="true"/></td>
+   </tr>
+   <tr style="background: white">
+     <td style="background: white">Max results:</td>
+     <td style="background: white"><input type="number" value="10" id="max_results" maxlength="5" 
+       style="color: #999999" disabled="true"/></td>
+   </tr>
+   <tr style="background: white">
+     <td style="background: white">Variable prefix:</td>
+     <td style="background: white"><input type="text" value="results" id="variable_prefix" maxlength="20" 
+       style="color: #999999" disabled="true"/>
+   </tr>
+   <tr style="background: white">
+     <td style="background: white"><input type="button" value="Generate code" id="generate_code" 
+       class="p-Widget jupyter-widgets jupyter-button widget-button" disabled="true"
+       onclick="generatePythonCode()"/>
+   </tr>
+</table>
+"""
+
+_CONCEPT_ROW_HTML_TEMPLATE = """
+  <tr id="row_{id}" onclick='selectConcept("{id}", {js_escaped_name}, {js_escaped_domain}, {js_escaped_vocabulary}, {standard})'>
+    <td>{id}</td>
+    <td style="text-align: left">{name}</td>
+    <td style="text-align: left">{code}</td>
+    <td style="text-align: left">{domain}</td>
+    <td style="text-align: left">{vocabulary}</td>
+    <td>{count}</td>
+  </tr>
+"""
+
 _VOCAB_DICT = {id: id for id in _VOCAB_IDS}
 
 def search_concepts(request):
@@ -73,7 +199,7 @@ def search_concepts(request):
     return response.items
 
 def get_concept_dict(concept):
-    return {f[0]: getattr(concept, f[1]) for f in _RESULT_FIELDS} 
+    return {f[0]: getattr(concept, f[1]) for f in _RESULT_FIELDS}
 
 def get_concepts_frame(request):
     concepts = search_concepts(request)
@@ -81,14 +207,24 @@ def get_concepts_frame(request):
                         columns = [f[0] for f in _RESULT_FIELDS])
 
 def display_concepts(request):
-    concepts_frame = get_concepts_frame(request)
-    s = concepts_frame.style.set_properties(**{'text-align': 'left'})
-    s = s.set_table_styles(
-        [{"selector": "th", "props": [("text-align", "left")]}]).hide_index()
-    display(HTML(s.render()))
+    concepts = search_concepts(request)
+    row_html = ''
+    for concept in concepts:
+      row_html += _CONCEPT_ROW_HTML_TEMPLATE.format(id=concept.concept_id,
+                                                    name=html.escape(concept.concept_name),
+                                                    js_escaped_name=json.dumps(concept.concept_name),
+                                                    code=html.escape(concept.concept_code),
+                                                    domain=html.escape(concept.domain_id),
+                                                    js_escaped_domain=json.dumps(concept.domain_id),
+                                                    vocabulary=html.escape(concept.vocabulary_id),
+                                                    js_escaped_vocabulary=json.dumps(concept.vocabulary_id),
+                                                    count=concept.count_value,
+                                                    standard='true' if concept.standard_concept else 'false')
+    table_html = _CONCEPT_TABLE_HTML_TEMPLATE % row_html
+    display(HTML(table_html))
 
 def display_concepts_fn(query, domain, vocabulary, concepts):
-    request = SearchConceptsRequest(query=query)
+    request = SearchConceptsRequest(query=query, max_results=10)
     if domain:
         request.domain = domain
     if concepts:
